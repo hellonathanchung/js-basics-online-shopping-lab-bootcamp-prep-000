56f2ccce86be1458c5e665775784222f27a000d6
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:06:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-  } else if (item != cart[i]) {
+  }
+  else if (item != cart[i]) {
     return "That item is not in your cart."
   }
 }
