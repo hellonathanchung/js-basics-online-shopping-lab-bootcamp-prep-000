96bad3f33632ac8734ba48b28ef6d512f602517c
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:39:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardnumber) {
-     if (typeof(cardNumber) === "undefined") {
+     if (typeof(cardnumber) === "undefined") {
     return "Sorry, we don't have a credit card on file for you."
   }
   else {
