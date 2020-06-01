687ee50539045549693f8e16c4887ccb2bfb6375
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:40:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardnumber) {
      if (typeof(cardnumber) === "undefined") {
-    console.log "Sorry, we don't have a credit card on file for you."
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
     console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
