eaf59fca1624b2282be08381de5e22f5c89bfb58
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardnumber) {
   if (cardnumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardnumber}.`;
-    cart = [];
+console.log `Your total cost is $${total()}, which will be charged to the card ${cardnumber}.`;
+cart = [];
+    
   } if (cardnumber === undefined) {
   return "Sorry, we don't have a credit card on file for you.";
   
