5341fe2b00c7f5a0c5807bfefa03e65379ca732a
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 00:38:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 function viewCart() {
   if (cart.length === 0 ) {
     return "Your shopping cart is empty."
-  } else (cart.length === 1) {
+  } else (cart.length === 1); {
     return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
   }
   }
