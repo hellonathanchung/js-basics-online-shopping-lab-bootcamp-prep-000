b28df6f8e91bcef9f5f16f9914a0d73f96353030
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:25:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ function placeOrder(cardnumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardnumber}.`
-  }
   cart = [];
+  }
 }
