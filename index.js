6e0e0858899479cbead382ce986d90bc9a32e3de
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 01:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,11 +47,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardnumber) {
-  if (!cardnumber) {
-    return "Sorry, we don't have a credit card on file for you."
-  } else {
+  if (cardnumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardnumber}.`
-  var cart = [];
-  }
-
-}
+  cart = [];
+  } else return "Sorry, we don't have a credit card on file for you."
+  } 
