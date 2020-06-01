9f4c5ebb394943700256e9c39dcaf2d77ec66a9b
Nathan Chung <chung.nathanh@gmail.com>
2020-06-01 00:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
 function addToCart(item) {
- // write your code here
-}
+ var price = Math.floor(Math.random() * 100) + 1;
+ var itemAndPrice = {itemName: `${item}`, itemPrice:price};
+ 
+ cart.push(itemAndPrice);
+ return itemAndPrice.itemName + " has been added to your cart."
 
+}
 function viewCart() {
-  // write your code here
+  if (cart.length === 0 ) {
+    return "Your shopping cart is empty."
+  } else (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
+  }
+  }
 }
-
 function total() {
   // write your code here
 }
